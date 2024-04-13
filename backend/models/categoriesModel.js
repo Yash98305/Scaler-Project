e4ae62a2c4ddd8eaa4df548df6da28d4648b209c
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2024-04-13 16:16:52 +0530

    2

--- backend/models/categoriesModel.js
+++ backend/models/categoriesModel.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 
-const accountSchema = new mongoose.Schema(
+const categoriesSchema = new mongoose.Schema(
     {
         userId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
         },
+
         name: {
             type: String,
         },
-        balance: {
-            type:String ,
-        },
+
         type: {
             type: String,
+            enum:["income", "expense"],
         },
-        status:{
+        
+        budget:{
             type: String,
-            enum:["active","inactive"],
         },
     },
     {
         timestamps: true,
     }
 );
-module.exports = mongoose.model("Account", accountSchema);
+module.exports = mongoose.model("Categories", categoriesSchema);
