cbe0b9ea68f1aae614b8570af0075ec1f7ae5c6d
Yash Patel <99255356+Yash98305@users.noreply.github.com>
2024-04-14 15:14:02 +0530

    a

--- frontend/src/pages/components/CategoryPage.jsx
+++ frontend/src/pages/components/CategoryPage.jsx
@@ -1,17 +1,16 @@
 import CategoryForm from './yash/CategoryForm'
-<<<<<<< HEAD
+
 import { Button } from '@mui/material'
-=======
+
 import CategoryCreate from './vansh/CategoryCreate';
 import * as React from 'react';
-import Button from '@mui/material/Button';
+
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
->>>>>>> 899fe055400965f483abf62b2f8e877e450dd5fd
 
 const CategoryPage = () => {
   return (
