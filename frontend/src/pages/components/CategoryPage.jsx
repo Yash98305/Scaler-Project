899fe055400965f483abf62b2f8e877e450dd5fd
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2024-04-14 15:01:05 +0530

    q

--- frontend/src/pages/components/CategoryPage.jsx
+++ frontend/src/pages/components/CategoryPage.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
 import CategoryForm from './yash/CategoryForm'
+import CategoryCreate from './vansh/CategoryCreate';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const CategoryPage = () => {
   return (
@@ -8,7 +16,10 @@ const CategoryPage = () => {
       <div className='category_page_left'>
         <CategoryForm/>
       </div>
-      <div className='category_page_right'></div>
+      <div className='category_page_right'>
+      <CategoryCreate/>
+
+      </div>
       </div>
       
     </>
