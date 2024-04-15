a3c2d966791766d36425d4c81cff56497b3c97b2
Yash Patel <99255356+Yash98305@users.noreply.github.com>
2024-04-15 13:58:08 +0530

    t

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -51,10 +51,10 @@ res.status(201).send({
 
 exports.getController = catchAsyncError(async(req,res,next)=>{
     const userId = req.user._id
-    const account = await Category.find({userId})
+    const account = await Account.find({userId})
     res.status(200).send({
         success: true,
-        message: "Categories fetched successfully",
-        categories
+        message: "Account fetched successfully",
+        account
     })
 })
