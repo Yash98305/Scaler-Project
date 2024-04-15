13fc27c7fba45094604e9ee7b6212dc677c2805f
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2024-04-15 14:25:31 +0530

    1

--- frontend/src/pages/components/Devendra/Record.jsx
+++ frontend/src/pages/components/Devendra/Record.jsx
@@ -1,28 +1,25 @@
 import React from 'react'
-<<<<<<< HEAD
 import '../../../css/record.css'
 function Record() {
   return (
     <div className='d'>
-        <div className='d1'>
-            <h3>Income</h3>
-            <h1>+ Rs 0</h1>
-        </div>
-        <div className='d2'>
-            <h3>Expense</h3>
-            <h1>+ Rs 0</h1>
-        </div>
-      
-=======
+      <div className='d1'>
+        <h3>Income</h3>
+        <h1>+ Rs 0</h1>
+      </div>
+      <div className='d2'>
+        <h3>Expense</h3>
+        <h1>+ Rs 0</h1>
+      </div>
+      </div>
 
-function Record() {
+      function Record() {
   return (
-    <div className=''>
-      <div></div>
-      <div></div>
->>>>>>> d6b880900270461e403e8a03430e6374f6d614f5
-    </div>
-  )
+      <div className=''>
+        <div></div>
+        <div></div>
+      </div>
+      );
 }
 
-export default Record
+      export default Record
