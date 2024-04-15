2014d73d84ffa0f4eed27542cbd58c62199eca4e
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2024-04-15 14:17:15 +0530

    1

--- frontend/src/pages/components/HomePage.jsx
+++ frontend/src/pages/components/HomePage.jsx
@@ -3,14 +3,33 @@ import CreditCard from './vansh/CreditCard'
 
 const HomePage = () => {
   return (
-    <div style={{width:"100%",height:"100%"}}>
+    <div style={{ width: "100%", height: "100%" , display:"flex",}}>
 
-    
-    Dashboard
-    <CreditCard/>
-    
+      <div className="leftCont" style={{border:"2px solid red",width:"60%",margin:"10px",borderRadius:"30px",padding:"10px"}}>
+
+        <div className="upper-leftCont" style={{ display: "flex" ,justifyContent:"space-around"}}>
+
+          <div style={{ border: "2px solid red" }}>
+            Dashboard
+            <CreditCard />
+          </div>
+
+          <div style={{ border: "2px solid red" }}>
+            Upcoming Payments
+          </div>
+
+        </div>
+
+        <div className="down-leftCont" style={{border:"2px solid red",borderRadius:"30px",marginTop:"40px"}}>
+          Recent Transaction
+        </div>
+      </div>
+      <div className="rightCont" style={{border:"2px solid red",width:"40%",margin:"10px",borderRadius:"30px"}}>
+
+      </div>
+
     </div>
-    
+
   )
 }
 
