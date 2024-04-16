5c9cc290c110c9899b4fe7ef79c6ca77b0a19f46
Yash Patel <99255356+Yash98305@users.noreply.github.com>
2024-04-16 13:20:01 +0530

    g

--- frontend/src/pages/Layout/Body.jsx
+++ frontend/src/pages/Layout/Body.jsx
@@ -10,7 +10,7 @@ import Category from "../Category";
 import Income from "../Income";
 import Budget from "../Budget";
 import Statistic from "../Statistic";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 import AccountBoxRoundedIcon from "@mui/icons-material/AccountBoxRounded";
 import PaidRoundedIcon from "@mui/icons-material/PaidRounded";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
@@ -19,14 +19,14 @@ import ArchiveRoundedIcon from "@mui/icons-material/ArchiveRounded";
 import InsightsRoundedIcon from "@mui/icons-material/InsightsRounded";
 import InventoryRoundedIcon from "@mui/icons-material/InventoryRounded";
 import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
-import NotificationsActiveRoundedIcon from '@mui/icons-material/NotificationsActiveRounded';
+import NotificationsActiveRoundedIcon from "@mui/icons-material/NotificationsActiveRounded";
 import AccountBalanceWalletRoundedIcon from "@mui/icons-material/AccountBalanceWalletRounded";
 import Search from "./Search";
 const Body = ({ obj }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const { auth, setAuth } = useAuth();
-console.log(auth);
+  console.log(auth);
   const objectreturn = (obj) => {
     return obj;
   };
@@ -41,75 +41,96 @@ console.log(auth);
   };
   const userId = JSON.parse(localStorage.getItem("auth")).user._id;
 
-
   return (
     <>
       {" "}
       <div className="home_con">
         <div>
           <div className="horizontal_nav">
-            <div style={{fontWeight:"bolder",fontSize:"30px"}}>CashSavvy</div>
-            <div><Search/></div>
+            <div style={{ fontWeight: "bolder", fontSize: "30px" }}>
+              CashSavvy
+            </div>
+            <div>
+              <Search />
+            </div>
             <div className="horizontal_nav_1">
-              <div style={{paddingRight:"20px"}}><NotificationsActiveRoundedIcon color="secondary"/></div>
-              <Avatar style={{border:"2px solid black",zIndex:"11 !important"}}  sx={{ width: 50, height: 50 }} src={`http://localhost:8000/api/v1/user/photo/${userId}`} alt="error" />
+              <div style={{ paddingRight: "20px" }}>
+                <NotificationsActiveRoundedIcon color="secondary" />
+              </div>
+              <Avatar
+                style={{ border: "2px solid black", zIndex: "11 !important" }}
+                sx={{ width: 50, height: 50 }}
+                src={`http://localhost:8000/api/v1/user/photo/${userId}`}
+                alt="error"
+              />
             </div>
           </div>
           <div className="home_content">
             <div className="vertical_nav">
               <ul>
+                <NavLink to="/home">
+                  <li>
+                    <HomeRoundedIcon color="secondary" />
+                    <span style={{ paddingLeft: "10px" }}>Overview</span>
+                  </li>
+                </NavLink>
+                <NavLink to="/category">
                 <li>
-                  <HomeRoundedIcon color="secondary" />{" "}
-                  <NavLink to="/home">Overview</NavLink>
+                  <InventoryRoundedIcon color="secondary" />
+                  <span style={{ paddingLeft: "10px"}}>Categories</span>
+                  
                 </li>
-                <li>
-                  <InventoryRoundedIcon color="secondary" />{" "}
-                  <NavLink to="/category">Categories</NavLink>
-                </li>
-                <li>
-                    <ArchiveRoundedIcon color="secondary" />
+                  </NavLink>
                   <NavLink to="/expenses">
-                    Expenses
-                  </NavLink>
-                </li>
+
                 <li>
-                    <AccountBalanceWalletRoundedIcon color="secondary" />
-                  <NavLink to="/income">
-                    Income
-                  </NavLink>
+                  <ArchiveRoundedIcon color="secondary" />
+                  <span>Expenses</span>
                 </li>
+                </NavLink>
+                <NavLink to="/income">
+
                 <li>
-                    <PaidRoundedIcon color="secondary" />
-                  <NavLink to="/budget">
-                    Budget
-                  </NavLink>
+                  <AccountBalanceWalletRoundedIcon color="secondary" />
+                  <span>Income</span>
                 </li>
+                </NavLink>
+                <NavLink to="/budget">
+
                 <li>
-                    <InsightsRoundedIcon color="secondary" />
-                  <NavLink to="/statistic">
-                    Statistics
-                  </NavLink>
+                  <PaidRoundedIcon color="secondary" />
+                  <span>Budget</span>
                 </li>
+                </NavLink>
+                <NavLink to="/statistic">
+
                 <li>
-                    <InsightsRoundedIcon color="secondary" />
-                  <NavLink to="/account">
-                    Account
-                  </NavLink>
+                  <InsightsRoundedIcon color="secondary" />
+                  <span>Statistics</span>
                 </li>
+                </NavLink>
+                <NavLink to="/account">
+
+                <li>
+                  <InsightsRoundedIcon color="secondary" />
+                  <span>Account</span>
+                </li>
+                </NavLink>
               </ul>
               <ul>
-                <li>
-                    <AccountBoxRoundedIcon color="secondary" />
-                  <NavLink to="/profile">
-                    Profile
-                  </NavLink>
+              <NavLink to="/profile">
+                <li style={{width:"150px"}}>
+                  <AccountBoxRoundedIcon color="secondary" />
+                  <span>Profile</span>
+                  
                 </li>
-                <li>
-                    <LogoutRoundedIcon color="secondary" />
+                </NavLink>
                   <NavLink to="/login" onClick={handleLogout}>
-                    Log Out
-                  </NavLink>
+                <li>
+                  <LogoutRoundedIcon color="secondary" />
+                  <span>Log Out</span> 
                 </li>
+                  </NavLink>
               </ul>
             </div>
             <div className="page">
