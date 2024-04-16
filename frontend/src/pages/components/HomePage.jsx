ec0a734c28dbd5268772b4320fda647737fc0b6e
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2024-04-16 17:27:47 +0530

    3

--- frontend/src/pages/components/HomePage.jsx
+++ frontend/src/pages/components/HomePage.jsx
@@ -86,7 +86,10 @@ const HomePage = () => {
               padding: "10px 0px",
             }}
           >
-            <Record />
+            <Record amount={1200} icon={<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><rect fill="none" height="24" width="24"/></g><g><g><rect height="7" width="3" x="4" y="10"/><rect height="7" width="3" x="10.5" y="10"/><rect height="3" width="20" x="2" y="19"/><rect height="7" width="3" x="17" y="10"/><polygon points="12,1 2,6 2,8 22,8 22,6"/></g></g></svg>
+} name={"Income"} style={{marginRight:"6"}}/>
+            <Record amount={1200} icon={<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><rect fill="none" height="24" width="24"/></g><g><g><rect height="7" width="3" x="4" y="10"/><rect height="7" width="3" x="10.5" y="10"/><rect height="3" width="20" x="2" y="19"/><rect height="7" width="3" x="17" y="10"/><polygon points="12,1 2,6 2,8 22,8 22,6"/></g></g></svg>
+} name={"Expense"} />
           </div>
         </div>
 
@@ -165,7 +168,7 @@ const HomePage = () => {
 
       <div
         className="rightCont"
-        style={{ border: "2px solid red", width: "40%" }}
+        style={{width: "35%",backgroundColor:"#EEEDEB",height:"93%",margin:"15px",borderRadius:"25px",marginLeft:"30px", marginTop:"22px" }}
       >
         <PieChart
           series={[
