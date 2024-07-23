3f2761d1fb3db31318eaae347a9db483ea5d5593
Yash Patel <99255356+Yash98305@users.noreply.github.com>
2024-07-23 23:51:47 +0530

    cf

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,11 @@ const expenseRoute = require("./routes/expenseRoute.js");
 const incomeRoute = require("./routes/incomeRoute.js");
 const accountRoute = require("./routes/accountRoute.js");
 dotenv.config()
-
 app.use(cors());
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/category', categoriesRoute);
@@ -29,16 +25,12 @@ app.use('/api/v1/income', incomeRoute);
 app.use('/api/v1/expense', expenseRoute);
 app.use('/api/v1/account', accountRoute);
 
-app.get('/api/v1', (req, res) => {
+app.get('/', (req, res) => {
     res.send({
         message: "welcome to scaler application"
     })
 });
 
-// All other GET requests not handled before will return the React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-});
 
 app.use(errorMiddleware);
 
